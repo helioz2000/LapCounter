84693e40f31e93b79ea39b3a72196d2ddb286095
helioz2000 <helioz2000@gmail.com>
2019-01-23 16:47:52 +1100

    work in progress

---
+++ lc_power/lc_power.h
@@ -0,0 +1,5 @@
+typedef enum BatteryType{
+  NONE,       // No battery connected
+  LEAD_ACID,  // Lead Acid
+  LI_ION      // LiIon battery
+};
--- lc_power/lc_power.ino
+++ lc_power/lc_power.ino
@@ -18,6 +18,23 @@
  * 12 - Analog Input - Voltage Batt1 (MCP6002 A)
  * 13 - not connected (Serial TX for commissioning)
  * 14 - GND
+ * 
+ * Functional description:
+ * Selects the supply from one of two connected batteries based on charge state.
+ * Measures and displays actual battery voltage, current and consumed power.
+ * The batteries can be either 12V Lead Acid or 18V LiIon (tool) batteries.
+ * The type of battery is automatically determined by the measured voltage.
+ * 
+ * Battery volateg ranges:
+ * Lead Acid: 
+ * voltage range: 12-14.7V. 
+ * At rest: 100% > 12.6V, 50%=12.4V, Dead Flat=12.0V
+ * 
+ * LiIon:
+ * packs consist of 10 cells in 2P5S configuration.
+ * volatge range: 15.0 - 21V (3.0V-4.2V per cell)
+ * 
+ * 
  */
 
 // enable for serial debugging
@@ -29,6 +46,7 @@
 #include <SoftwareSerial.h>
 #endif
 
+#include "lc_power.h"                   // typedefs need to be in a header file
 #include "TinyWireM.h"                  // I2C Master lib for ATTinys which use USI
 #include "LiquidCrystal_attiny.h"       // for LCD w/ GPIO MODIFIED for the ATtiny85
 #include "Smoothed.h"                   // to filter ACS712 readings
@@ -36,7 +54,6 @@
 #define LCD_I2C_ADDR     0x3F              // (PCA8574A A0-A2 @5V) typ. A0-A3 Gnd 0x20 / 0x38 for A
 const byte LCD_ROWS = 4;
 const byte LCD_COLUMNS = 20;
-
 
 // I/O Pin definitions
 #define BAT1_SEL_PIN 10   // pin 2
@@ -66,7 +83,21 @@ const int acs712_zero_offset_i1 = -15;         // ACS712 zero point offset from 
 const int acs712_zero_offset_i2 = -20;         // ACS712 zero point offset from VCC/2 [mV]
 
 // Analog readings
-int bat1_I_mA, bat2_I_mA, bat1_V_mV, bat2_V_mV;
+int bat_I_mA[2], bat_V_mV[2];
+
+const int ANALOG_READ_INTERVAL = 100;       // ms between analog update
+const int DISPLAY_UPDATE_INTERVAL = 250;    // ms between display update
+const int PROCESS_INTERVAL = 500;           // ms between logic processing
+
+// Battery variables
+#define B1 0    // index for arrays
+#define B2 1
+
+BatteryType bat_type[] = { NONE, NONE };
+bool bat_low[] = { false, false };
+unsigned long bat_low_time[] = { 0, 0 };
+
+unsigned long nextAnalogRead, nextDisplayUpdate, nextProcess;
 
 void setup() {
   // For debugging
@@ -103,6 +134,38 @@ void setup() {
 #endif
   delay(2000);
   lcd.clear();
+
+  // start update loops
+  nextAnalogRead = millis()+ANALOG_READ_INTERVAL;
+  nextDisplayUpdate = millis()+DISPLAY_UPDATE_INTERVAL;
+  nextProcess = millis()+PROCESS_INTERVAL;
+}
+
+BatteryType getBatteryType(int voltage) {
+  if (voltage < 10500) return NONE;
+  if (voltage > 14900) return LI_ION;
+  return LEAD_ACID;
+}
+
+bool checkBatteryLow(int bat_index) {
+  int lowVoltage;
+  switch (bat_type[bat_index])  {
+  case NONE:
+    return false;
+  case LI_ION:
+    lowVoltage = 15000;
+    break;
+  case LEAD_ACID:
+    lowVoltage = 11500;
+    break;
+  }
+  return false;
+}
+
+// process logic
+void process() {
+  bat_type[B1] = getBatteryType(bat_V_mV[B1]);
+  bat_type[B2] = getBatteryType(bat_V_mV[B2]);
 }
 
 // convert raw analog reading to mV
@@ -123,37 +186,40 @@ void readAnalogs(void) {
   
   int analogIn_mV = (int)analogVoltage(bat1_I_raw.get()); 
 #ifdef DEBUG_SHOW_AI_V
-  bat1_I_mA = analogIn_mV;
+  bat_I_mA[B1] = analogIn_mV;
 #else
-  bat1_I_mA = (analogIn_mV - (vcc_mV / 2) - acs712_zero_offset_i1) * mA_per_mV;
+  bat_I_mA[B1] = (analogIn_mV - (vcc_mV / 2) - acs712_zero_offset_i1) * mA_per_mV;
 #endif
 
   analogIn_mV = (int)analogVoltage(bat2_I_raw.get()); 
 #ifdef DEBUG_SHOW_AI_V
-  bat2_I_mA = analogIn_mV;
+  bat_I_mA[B2] = analogIn_mV;
 #else
-  bat2_I_mA = (analogIn_mV - (vcc_mV / 2) - acs712_zero_offset_i2) * mA_per_mV;
+  bat_I_mA[B2] = (analogIn_mV - (vcc_mV / 2) - acs712_zero_offset_i2) * mA_per_mV;
 #endif
 
   analogIn_mV = (int)readAnalogVoltage(BAT1_V_PIN);
 #ifdef DEBUG_SHOW_AI_V
-  bat1_V_mV = analogIn_mV;
+  bat_V_mV[B1] = analogIn_mV;
 #else
-  bat1_V_mV = int(float(analogIn_mV - 913) / 0.337) + 13000;
+  bat_V_mV[B1] = int(float(analogIn_mV - 913) / 0.337) + 13000;
 #endif
 
   analogIn_mV = (int)readAnalogVoltage(BAT2_V_PIN);
 #ifdef DEBUG_SHOW_AI_V
-  bat2_V_mV = analogIn_mV;
+  bat_V_mV[B2] = analogIn_mV;
 #else
-  bat2_V_mV = int(float(analogIn_mV - 913) / 0.337) + 13000;
+  bat_V_mV[B2] = int(float(analogIn_mV - 913) / 0.337) + 13000;
 #endif
 }
 
 void displayValues () {
+#ifdef DEBUG_SHOW_AI_RAW
+  displayRawValues();
+#else  
   lcd.setCursor(0,0);
   lcd.print("I1=");
-  lcd.print(bat1_I_mA);
+  lcd.print(bat_I_mA[B1]);
 #ifdef DEBUG_SHOW_AI_V
   lcd.print("mV");
 #else
@@ -162,7 +228,7 @@ void displayValues () {
 
   lcd.setCursor(10,0);
   lcd.print("V1=");
-  lcd.print(bat1_V_mV);
+  lcd.print(bat_V_mV[B1]);
 #ifdef DEBUG_SHOW_AI_V
   lcd.print("mV");
 #else
@@ -171,7 +237,7 @@ void displayValues () {
   
   lcd.setCursor(0,1);
   lcd.print("I2=");
-  lcd.print(bat2_I_mA);
+  lcd.print(bat_I_mA[B2]);
 #ifdef DEBUG_SHOW_AI_V
   lcd.print("mV");
 #else
@@ -180,11 +246,12 @@ void displayValues () {
 
   lcd.setCursor(10,1);
   lcd.print("V2=");
-  lcd.print(bat2_V_mV);
+  lcd.print(bat_V_mV[B2]);
 #ifdef DEBUG_SHOW_AI_V
   lcd.print("mV");
 #else
   lcd.print("  ");
+#endif
 #endif
 }
 
@@ -211,9 +278,6 @@ void displayRawValues () {
   lcd.print(analogRead(BAT2_V_PIN));
   lcd.print("  ");
 
-
-  bat1_I_raw.add(analogRead(BAT1_I_PIN));
-  bat2_I_raw.add(analogRead(BAT2_I_PIN));
   lcd.setCursor(0,2);
   lcd.print("I1 Filtered =");
   lcd.print(bat1_I_raw.get());
@@ -226,15 +290,27 @@ void displayRawValues () {
 }
 #endif
 
+
+
 void loop() {
 #ifdef DEBUG_SERIAL  
   Serial.println("lc_power is operational");
 #endif
-  delay(100);
-#ifdef DEBUG_SHOW_AI_RAW
-  displayRawValues();
-#else
-  readAnalogs();
-  displayValues();
-#endif
+
+  // Update analog values
+  if (millis() >= nextAnalogRead) {
+    nextAnalogRead = millis() + ANALOG_READ_INTERVAL;
+    readAnalogs();
+  }
+
+  // Update display
+  if (millis() >= nextDisplayUpdate) {
+    nextDisplayUpdate = millis() + DISPLAY_UPDATE_INTERVAL;
+    displayValues();
+  }
+
+  if (millis() >= nextProcess) {
+    nextProcess = millis() + PROCESS_INTERVAL;
+    process();
+  }
 }
